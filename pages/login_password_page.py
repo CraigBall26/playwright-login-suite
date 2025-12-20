23ea14ade7b4118e686f9caaa09fa4d550b5b8c4
Craig Ball <craigdavidball14@gmail.com>
2025-12-20 19:59:08 +0000

    Add LoginPasswordPage

---
+++ pages/login_password_page.py
@@ -0,0 +1,15 @@
+# Second step/page of a two-step login flow.
+# Handles entering the password and submitting the login form.
+
+
+class LoginPasswordPage:
+    def __init__(self, page: Page):
+        self.page = page
+        self.password_input = page.locator("input[type='password']")
+        self.submit_button = page.get_by_role("button", name="Sign in")
+
+    def enter_password(self, password: str):
+        self.password_input.fill(password)
+
+    def submit(self):
+        self.submit_button.click()
