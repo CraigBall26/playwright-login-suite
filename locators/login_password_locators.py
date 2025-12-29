8a504667a1d533c68bda46ba65f78c6c4f6b4189
Craig Ball <craigdavidball14@gmail.com>
2025-12-29 15:42:05 +0000

    Error locator CI fixes

--- locators/login_password_locators.py
+++ locators/login_password_locators.py
@@ -6,14 +6,21 @@ CONTINUE_BUTTON = "button[type='submit']"
 # Toggles
 SHOW_PASSWORD_TOGGLE = "[data-testid='password-visibility-toggle']"
 
-# Error messages
-PASSWORD_ERROR_SELECTORS = [
+# Edit email button
+EDIT_EMAIL_BUTTON = "[data-link-name='edit-username']"
+
+# Error selectors with fallback logic
+PASSWORD_ERROR_SELECTORS = (
+    # Known incorrect-password variants
     "div.c4de1c3f0.c040bb180:has-text('incorrect')",
     "div.c092c43cb:has-text('incorrect')",
     "[data-testid='login-error']",
     ".auth0-global-message-error",
     ".alert-error",
-]
-
-# Edit email button
-EDIT_EMAIL_BUTTON = "[data-link-name='edit-username']"
+    # Unknown-user variants (CI often uses these)
+    "div.auth0-global-message.auth0-global-message-error",
+    "[data-testid='error']",
+    "div[class*='error']:has-text('incorrect')",
+    "p:has-text('We can')",
+    "[data-qa='error-message']",
+)
